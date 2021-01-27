a73b8fde0bbadf125b347b85d4bc9ab14fca7f20
ChristianChesire <christian.chesire@strathmore.edu>
2021-01-27 14:29:33 +0000

    Working with strings

---
+++ 1.code/strings.rb
@@ -0,0 +1,19 @@
+# We use strings when working with text data
+# put-s adds a new line print statement 
+puts "Hello World"
+# p doesn't return nill
+p "Hello World"
+# print doesn't add a new line to print statement
+# We add \n for newline
+print "Hello World\n"
+
+# A variables is a reference we give to some location in memory
+greeting = "Hello Chris"
+puts greeting 
+
+# Defining a method to take in argument
+def say_hello(thing_to_say)
+  puts thing_to_say
+end 
+# Calling the method
+say_hello "Hello Galaxy"
