cf1b3b6d99ced401a2b54269984087c51efaa0e1
ChristianChesire <christian.chesire@strathmore.edu>
2021-01-27 14:01:19 +0000

    Working with arrays

---
+++ 1.code/first_ruby.rb
@@ -0,0 +1,9 @@
+# create array address
+address = [1,2,3,4,5,6,7,8,9]
+# print out array
+p address
+# print value in address 5
+p address[5]
+# Reverse array
+new_address = address.reverse!
+p new_address
